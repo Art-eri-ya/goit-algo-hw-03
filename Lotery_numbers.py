0e0a479f49ba32969db6b71f33266f54c12c4910
Artem <86430417+Art-eri-ya@users.noreply.github.com>
2026-03-23 16:27:34 -0400

    Update Lotery_numbers.py
    I added method "sorted" before returning the numers, so the list appears sorted. Next, I spoted the incompleteness of my condition and modified it bu using connextor "and". Let me know if it runs correctly now. Thanks, for your time.

--- Lotery_numbers.py
+++ Lotery_numbers.py
@@ -3,7 +3,7 @@ import random
 def get_numbers_ticket(min, max, quantity):
     numbers = []
     our_range = range(min, max+1)
-    if min >= 1 and max <=1000:
+    if min in range(1,1000) and max in range(1,1000) and quantity <= len(our_range):
         numbers = random.sample(our_range, quantity)
 
     else:
@@ -12,5 +12,7 @@ def get_numbers_ticket(min, max, quantity):
 
 
 print(get_numbers_ticket(1, 49, 6))
-print(get_numbers_ticket(0, 49, 6))
-print(get_numbers_ticket(1, 1001, 6))
+print(get_numbers_ticket(-10, 10, 5))
+print(get_numbers_ticket(1000, 1200, 10))
+print(get_numbers_ticket(10, 4, 5))
+print(get_numbers_ticket(10, 14, 6))
